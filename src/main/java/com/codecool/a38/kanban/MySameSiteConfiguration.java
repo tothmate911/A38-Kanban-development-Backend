31b9ecde01def9064993b55d8f57f917dbbef446
tothmate911 <tothmate911@gmail.com>
2022-05-14 10:22:54 +0200

    Revert "Delete MySameSiteConfiguration.java"
    This reverts commit 351e907d382eb909be0e0648203ba1efb0597da6.

---
+++ src/main/java/com/codecool/a38/kanban/MySameSiteConfiguration.java
@@ -0,0 +1,15 @@
+package com.codecool.a38.kanban;
+
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySameSiteConfiguration {
+
+    @Bean
+    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofNone();
+    }
+
+}
